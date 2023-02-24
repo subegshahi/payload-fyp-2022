2709ce85e72917034cb79a94a07a029579bacd09
Subeg <84019079+subegshahi@users.noreply.github.com>
2023-02-24 16:18:12 +0545

    Update ContractorHome.jsx

--- client/src/pages/contractor-home/ContractorHome.jsx
+++ client/src/pages/contractor-home/ContractorHome.jsx
@@ -3,16 +3,16 @@ import { ContractorForm, Header } from "../imports";
 
 export const ContractorHome = () => {
   return (
-    <div>
+    <>
       <div className="bg-[#114c79]">
         <Header />
       </div>
 
-      <div className="mx-auto flex min-h-screen max-w-2xl flex-col justify-center px-5">
-        <div className="mt-10">
+      <main className="mx-auto flex min-h-screen max-w-2xl flex-col justify-center px-5">
+        <div>
           <ContractorForm />
         </div>
-      </div>
-    </div>
+      </main>
+    </>
   );
 };
