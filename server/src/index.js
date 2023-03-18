22a2dd6acb14e19179fa255f0977a04a7cbff6d4
Subeg <84019079+subegshahi@users.noreply.github.com>
2023-03-18 16:20:50 +0545

    feat: prisma schema

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,24 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { userRouter } from "./routes/users.js";
+import { prisma } from "../prisma/prisma-client.js";
 
 const app = express();
 
+// Middlewears
 app.use(express.json());
 app.use(cors());
 app.use("/auth", userRouter);
+
+// Prisma main function for testing db
+const main = async () => {
+  const user = await prisma.user.findMany();
+  const contractor = await prisma.contractor.findMany();
+
+  console.log({ contractor });
+};
+
+main();
 
 // Listening for request
 app.listen(process.env.PORT, () => {
