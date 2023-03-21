ded5d18c1ff18dd1c40514cf9b60001640ebaf00
Subeg <84019079+subegshahi@users.noreply.github.com>
2023-03-22 01:35:01 +0545

    feat: booking api and ticket api

--- server/src/routes/booking.js
+++ server/src/routes/booking.js
@@ -4,7 +4,19 @@ import { prisma } from "../../prisma/prisma-client.js";
 const router = express.Router();
 
 router.post("/booking", async (req, res) => {
-  res.json({ message: "booking endpoint called" });
+  const { from, to, departureDate, adultPassengerNumber, childPassengerNumber } = req.body;
+
+  const booking = await prisma.booking.create({
+    data: {
+      from,
+      to,
+      departureDate,
+      adultPassengerNumber,
+      childPassengerNumber,
+    },
+  });
+
+  res.json(booking);
 });
 
 router.get("/booking", async (req, res) => {
--- server/src/routes/ticket.js
+++ server/src/routes/ticket.js
@@ -4,7 +4,23 @@ import { prisma } from "../../prisma/prisma-client.js";
 const router = express.Router();
 
 router.post("/ticket", async (req, res) => {
-  res.json({ message: "ticket endpoint called" });
+  const { airlines, from, to, takeOffTime, landingTime, flightDuration, totalPassengerSeat, fare } =
+    req.body;
+
+  const ticket = await prisma.ticket.create({
+    data: {
+      airlines,
+      from,
+      to,
+      takeOffTime,
+      landingTime,
+      flightDuration,
+      totalPassengerSeat: parseInt(totalPassengerSeat),
+      fare: parseInt(fare),
+    },
+  });
+
+  res.json(ticket);
 });
 
 router.get("/ticket", async (req, res) => {
