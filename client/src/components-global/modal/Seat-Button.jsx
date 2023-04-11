b16a96b6844bbcebecf2708b934f49d6a5da513c
Subeg <84019079+subegshahi@users.noreply.github.com>
2023-04-12 00:17:03 +0545

    Update Seat-Button.jsx

--- client/src/components-global/modal/Seat-Button.jsx
+++ client/src/components-global/modal/Seat-Button.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const SeatButton = ({ SeatId }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
   return (
-    <button className="inline-block h-16 w-16 rounded-md bg-gray-200 p-3 font-semibold text-gray-700 transition-all hover:scale-110 hover:bg-gray-600/70 hover:text-white">
+    <button
+      className={`inline-block h-16 w-16 rounded-md bg-gray-200 p-3 font-semibold text-gray-700 transition-all hover:scale-110 hover:bg-gray-600/70 hover:text-white ${
+        isClicked ? "bg-blue-500 text-white" : ""
+      }`}
+      onClick={handleClick}
+    >
       {SeatId}
     </button>
   );
